fe66c24f258ca242292f080ce1cab2f94c858d10
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-02-02 18:34:44 +0200

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,12 +1,14 @@
 require './person'
 
 class Student < Person
+  attr_reader :classroom
 
-attr_reader :classroom
+
 
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    @parent_permission = parent_permission
   end
 
   def classroom=(classroom)
